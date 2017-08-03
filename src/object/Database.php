8132fcb5401bdea85a75407d882765727fe5b59d
Nana Kwame <9naquame@gmail.com>
2017-08-03 20:39:46 +0000

    Database objevt underway

--- src/object/Database.php
+++ src/object/Database.php
@@ -1,8 +1,26 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Datastore\Object;
 
+class Database extends \Datastore\Object
+{
+    public function __construct($params)
+    {
+        parent::__construct('database');
+        
+    }
+    
+    public function hasSchema()
+    {
+    }
+
+    public function addSchema($schema)
+    {
+        
+    }
+    
+    public function addTable($table)
+    {
+        
+    }
+}
