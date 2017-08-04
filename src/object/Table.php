7ce390f357af962925a8b8790748dbcc08376542
Nana Kwame <9naquame@gmail.com>
2017-08-04 22:52:23 +0000

    Table underway

--- src/object/Table.php
+++ src/object/Table.php
@@ -1,8 +1,13 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Datastore\Object;
 
+class Table extends Datastore\Object
+{
+    public function __construct($params)
+    {
+        parent::__construct('table');
+        
+    }
+    
+}
