4b69c3b16b0278e398febaf9ae644b65cf2933e8
Nana Kwame <9naquame@gmail.com>
2017-08-05 23:56:38 +0000

    added Column class

---
+++ src/object/Column.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Datastore\Object;
+
+class Column extends \Datastore\Object
+{
+    
+    public function __construct()
+    {
+        parent::__construct('column');
+    }
+           
+}
