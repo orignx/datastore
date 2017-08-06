a921a1aa2498e92c84f1049f6a5d6e39b200c4be
Nana Kwame <9naquame@gmail.com>
2017-08-06 23:59:31 +0000

    added Key object

--- src/object/Key.php
+++ src/object/Key.php
@@ -1,8 +1,13 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Datastore\Object;
 
+class Key extends \Datastore\Object
+{
+    
+    public function __construct()
+    {
+        parent::__construct('key');
+    }
+           
+}
