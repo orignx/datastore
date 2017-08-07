ab37cb3a438cc3f5473dd2b137e62e8d87f10358
Nana Kwame <9naquame@gmail.com>
2017-08-07 00:05:51 +0000

    index added

--- src/object/Index.php
+++ src/object/Index.php
@@ -1,8 +1,11 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Datastore\Object;
 
+class Index extends \Datastore\Object
+{
+    public function __construct()
+    {
+        parent::__construct('index');
+    }
+}
