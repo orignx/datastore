0abc3d346b8780ae8ac9a34b5c9941ff83ba9bc0
Nana Kwame <9naquame@gmail.com>
2017-08-08 20:01:44 +0000

    Role class underway

--- src/object/Role.php
+++ src/object/Role.php
@@ -1,8 +1,12 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Datastore\Object;
 
+class Role extends \Datastore\Object
+{
+    public function __construct()
+    {
+        parent::__construct('role');
+        
+    }
+}
