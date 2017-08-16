dc60a0ce176544983c8810070add6c37eb2d7c48
Nana Kwame <9naquame@gmail.com>
2017-08-16 18:44:51 +0000

    database driver set

--- src/Database.php
+++ src/Database.php
@@ -4,12 +4,25 @@ namespace orignx\datastore;
 
 class Database extends \orignx\datastore\Object
 {
+    private $driver;
+    private $tables;
+    private $schemas;
+    private $columns;
+    
     public function __construct($params)
     {
         parent::__construct('database');
+        $name = ucwords($params['name']);
+        $reflection = new \ReflectionClass("\\orignx\\datastore\\driver\\{$params['driver']}\\database\\{$name}");
         
+        $this->driver = $reflection->newInstance($params['name'], $params['config']);
     }
     
+    public function getDriver()
+    {
+        return $this->driver;
+    }
+
     public function hasSchema()
     {
     }
